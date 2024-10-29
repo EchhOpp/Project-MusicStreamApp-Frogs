99033dc31a162e698bd2ce676a010ae0adece026
EchhOpp <ngthluan285@gmail.com>
2024-10-29 14:03:02 +0700

    Add: Screen-GetStart

---
+++ Project-MusicStreamApp-Frogs/app/_layout.js
@@ -0,0 +1,15 @@
+import { StyleSheet, SafeAreaView } from 'react-native'
+import React from 'react'
+import GetStart from './sign_in/GetStart'
+
+const _layout = () => {
+  return (
+    <SafeAreaView>
+      <GetStart></GetStart>
+    </SafeAreaView>
+  )
+}
+
+export default _layout
+
+const styles = StyleSheet.create({})
---
+++ Project-MusicStreamApp-Frogs/app/sign_in/GetStart.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
+import React from 'react';
+import styles from './styles/GetStart';
+
+const GetStart = () => {
+  return (
+    <ImageBackground 
+      source={require('../../assets/images/background_sign.png')} 
+      style={styles.container}
+    >
+      <View style={styles.body}>
+        <View style={styles.context}>
+          <Text style={[styles.colortext, styles.fonttext30, styles.boldtext]} >Open</Text>
+          <Text style={[styles.colortext, styles.fonttext30, styles.boldtext]}>the world of music</Text>
+        </View>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={[styles.colortext, styles.fonttext18]}>Get Start</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default GetStart;
