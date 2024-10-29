7c9bb4744fa51664583df4eb7857b01dd3e6b68e
EchhOpp <ngthluan285@gmail.com>
2024-10-29 21:13:39 +0700

    Add: Constants Popins

--- Project-MusicStreamApp-Frogs/constants/Colors.ts
+++ Project-MusicStreamApp-Frogs/constants/Colors.ts
@@ -1,8 +1,4 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
+// Objective: Define the color palette for the application
 export const Colors = {
   primary: {
     main: '#DF3131',
