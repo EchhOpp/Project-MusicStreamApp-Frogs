c1ecdd9fb8b99da94c7e6080b73e036985645aad
phan dinh thai <phandinhthai012@gmail.com>
2024-11-07 02:49:22 +0700

    Add: Screen-SignUp-Step2

--- Project-MusicStreamApp-Frogs/app/_layout.js
+++ Project-MusicStreamApp-Frogs/app/_layout.js
@@ -4,11 +4,12 @@ import GetStart from './sign_in/GetStart'
 import SignIn from './sign_in/SignIn'
 import SignUp from './sign_in/SignUp'
 import SignInStep1 from './sign_in/SignInStep1'
+import SignInStep2 from './sign_in/SignInStep2'
 
 const _layout = () => {
   return (
     <SafeAreaView>
-      <SignInStep1></SignInStep1>
+      <SignInStep2></SignInStep2>
     </SafeAreaView>
   )
 }
