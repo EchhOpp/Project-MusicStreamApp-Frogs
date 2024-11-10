5f92fc63048f86810d103d031b86339753fd3fac
phan dinh thai <phandinhthai012@gmail.com>
2024-11-10 17:04:46 +0700

    Update: Btn signin

--- Project-MusicStreamApp-Frogs/app/_layout.js
+++ Project-MusicStreamApp-Frogs/app/_layout.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Navigation from './navigation/Navigation';
+import Navigation from './Navigation/Navigation';
 import GetStartCount from './screens/get_start/GetStartCount';
 
 const App = () => {
--- Project-MusicStreamApp-Frogs/app/screens/sign_in/SignIn.js
+++ Project-MusicStreamApp-Frogs/app/screens/sign_in/SignIn.js
@@ -1,23 +1,71 @@
-import {StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, TextInput} from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, TextInput } from 'react-native'
 import React from 'react'
-import styles  from './styles/SignIn'
+import { useState } from 'react'
+import styles from './styles/SignIn'
 
 const SignIn = (
     { navigation }
 ) => {
-  return (
-    <SafeAreaView style={styles.container}>
+    const [userName, setUserName] = useState("")
+    const [passWord, setPassWord] = useState("")
+    const [userNameBorderColor, setUserNameBorderColor] = useState('#7e7e7e');
+    const [passwordBorderColor, setPasswordBorderColor] = useState('#7e7e7e');
+    const [showError, setShowError] = useState(false);
+
+    const handleSignIn = () => {
+        let valid = true
+        if (userName.trim() === "") {
+            setUserNameBorderColor('#fb4343');
+            valid = false;
+        } else {
+            setUserNameBorderColor('#7e7e7e');
+        }
+        if (passWord.trim() === "") {
+            setPasswordBorderColor('#fb4343');
+            valid = false;
+        } else {
+            setPasswordBorderColor('#7e7e7e');
+        }
+        if (valid) {
+            navigation.navigate('SignInStep1');
+            setShowError(false)
+            
+        }else {
+            setShowError(true)
+        }
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
             <View style={styles.body}>
                 <View style={styles.viewInputs}>
-                    <TextInput style={[styles.input]} placeholder="Username or Email address" placeholderTextColor="#7e7e7e" />
-                    <TextInput style={[styles.input]} placeholder="Password" placeholderTextColor="#7e7e7e" />
+                    <TextInput
+                        style={[styles.input, { borderColor: userNameBorderColor }]}
+                        onChangeText={(text) => { setUserName(text) }}
+                        placeholder="Username or Email address"
+                        placeholderTextColor='#7e7e7e' //{borderColor ==='#7e7e7e'?'#7e7e7e':'#d42424'}
+                    />
+                    <TextInput
+                        style={[styles.input,{borderColor:passwordBorderColor}]}
+                        onChangeText={(text) => { setPassWord(text) }}
+                        placeholder="Password"
+                        placeholderTextColor="#7e7e7e" />
+                    {showError && (<View style={styles.viewerror}>
+                        <Image style={styles.imgerror} source={require('../../../assets/images/Information-circle.png')}/>
+                        <Text style={[styles.txterror]}>Username or Password is null</Text>
+                    </View>
+                    )}
+
+
                     <TouchableOpacity style={styles.btn}
-                        onPress={() => navigation.navigate('SignInStep1') }
+                        onPress={() => {
+                            handleSignIn()
+                        }}
                     >
                         <Text style={[styles.colortext2, styles.fonttext16]}>Sign in</Text>
                     </TouchableOpacity>
                     <TouchableOpacity>
-                    <Text style={[styles.colortext, styles.fonttext16]}>Forget password?</Text>
+                        <Text style={[styles.colortext, styles.fonttext16]}>Forget password?</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -28,13 +76,13 @@ const SignIn = (
                     <Image source={require('../../../assets/images/Line24.png')} style={styles.footerImg} />
                 </View>
                 <TouchableOpacity style={styles.btn}
-                    onPress={() => navigation.navigate('SignUp') }
+                    onPress={() => navigation.navigate('SignUp')}
                 >
-                    <Text style={[styles.colortext, styles.fonttext16,styles.boldtext]}>Sign up for free</Text>
+                    <Text style={[styles.colortext, styles.fonttext16, styles.boldtext]}>Sign up for free </Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
-  )
+    )
 }
 
 export default SignIn
