783753049a356ab918925b3d41331c70c6e9acfc
EchhOpp <ngthluan285@gmail.com>
2024-11-12 17:08:45 +0700

    Add: Login with email & password on firebase for Sign In

--- Project-MusicStreamApp-Frogs/app/screens/sign_in/SignIn.js
+++ Project-MusicStreamApp-Frogs/app/screens/sign_in/SignIn.js
@@ -1,19 +1,20 @@
-import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
-import { useState } from 'react'
-import styles from './styles/SignIn'
+// SignIn.js
+import { Text, View, SafeAreaView, TouchableOpacity, Image, TextInput } from 'react-native';
+import { useState, useEffect } from 'react';
+import styles from './styles/SignIn';
 
-const SignIn = (
-    { navigation }
-) => {
-    const [userName, setUserName] = useState("")
-    const [passWord, setPassWord] = useState("")
+// Services Firebase
+import AuthSignIn from '../../../services/AuthSignIn';
+
+const SignIn = ({ navigation }) => {
+    const [userName, setUserName] = useState("");
+    const [passWord, setPassWord] = useState("");
     const [userNameBorderColor, setUserNameBorderColor] = useState('#7e7e7e');
     const [passwordBorderColor, setPasswordBorderColor] = useState('#7e7e7e');
     const [showError, setShowError] = useState(false);
 
     const handleSignIn = () => {
-        let valid = true
+        let valid = true;
         if (userName.trim() === "") {
             setUserNameBorderColor('#fb4343');
             valid = false;
@@ -26,14 +27,22 @@ const SignIn = (
         } else {
             setPasswordBorderColor('#7e7e7e');
         }
+
         if (valid) {
-            navigation.navigate('SignInStep1');
-            setShowError(false)
-            
-        }else {
-            setShowError(true)
+            AuthSignIn(userName, passWord)
+                .then((user) => {
+                    console.log('User signed in:', user);
+                    navigation.navigate('SignInStep1');
+                    setShowError(false);
+                })
+                .catch((error) => {
+                    alert("Username or Password is incorrect");
+                    setShowError(true);
+                });
+        } else {
+            setShowError(true);
         }
-    }
+    };
 
     return (
         <SafeAreaView style={styles.container}>
@@ -43,25 +52,23 @@ const SignIn = (
                         style={[styles.input, { borderColor: userNameBorderColor }]}
                         onChangeText={(text) => { setUserName(text) }}
                         placeholder="Username or Email address"
-                        placeholderTextColor='#7e7e7e' //{borderColor ==='#7e7e7e'?'#7e7e7e':'#d42424'}
+                        placeholderTextColor='#7e7e7e'
                     />
                     <TextInput
-                        style={[styles.input,{borderColor:passwordBorderColor}]}
+                        style={[styles.input, { borderColor: passwordBorderColor }]}
                         onChangeText={(text) => { setPassWord(text) }}
                         placeholder="Password"
-                        placeholderTextColor="#7e7e7e" />
-                    {showError && (<View style={styles.viewerror}>
-                        <Image style={styles.imgerror} source={require('../../../assets/images/Information-circle.png')}/>
-                        <Text style={[styles.txterror]}>Username or Password is null</Text>
-                    </View>
+                        secureTextEntry={true}
+                        placeholderTextColor="#7e7e7e"
+                    />
+                    {showError && (
+                        <View style={styles.viewerror}>
+                            <Image style={styles.imgerror} source={require('../../../assets/images/Information-circle.png')} />
+                            <Text style={[styles.txterror]}>Username or Password is null</Text>
+                        </View>
                     )}
 
-
-                    <TouchableOpacity style={styles.btn}
-                        onPress={() => {
-                            handleSignIn()
-                        }}
-                    >
+                    <TouchableOpacity style={styles.btn} onPress={handleSignIn}>
                         <Text style={[styles.colortext2, styles.fonttext16]}>Sign in</Text>
                     </TouchableOpacity>
                     <TouchableOpacity>
@@ -75,16 +82,12 @@ const SignIn = (
                     <Text style={[styles.colortext2, styles.fonttext14]}>Don't have an account?</Text>
                     <Image source={require('../../../assets/images/Line24.png')} style={styles.footerImg} />
                 </View>
-                <TouchableOpacity style={styles.btnfooter}
-                    onPress={() => navigation.navigate('SignUp')}
-                >
-                    <Text style={[styles.colortext, styles.fonttext16]}>Sign up for free </Text>
+                <TouchableOpacity style={styles.btnfooter} onPress={() => navigation.navigate('SignUp')}>
+                    <Text style={[styles.colortext, styles.fonttext16]}>Sign up for free</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default SignIn
-
-// const styles = StyleSheet.create({})
+export default SignIn;
---
+++ Project-MusicStreamApp-Frogs/services/AuthSignIn.js
@@ -0,0 +1,25 @@
+// Lấy thông tin từ form đăng ký và gọi hàm tạo tài khoản người dùng mới
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import { auth } from "../config/firebaseConfig";
+
+/**
+ * Hàm để tạo tài khoản người dùng mới
+ * @param {string} email - Địa chỉ email của người dùng
+ * @param {string} password - Mật khẩu của người dùng
+ * @returns {Promise} - Trả về một Promise chứa thông tin người dùng hoặc lỗi
+ */
+const signInUser = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      return user;
+    })
+    .catch((error) => {
+      // Xử lý lỗi đăng ký
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      throw new Error(`Error ${errorCode}: ${errorMessage}`);
+    });
+};
+
+export default signInUser;
