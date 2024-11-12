1516ef463df19068edddf3f7dbc1cacd21e4b78b
phan dinh thai <phandinhthai012@gmail.com>
2024-11-13 02:17:13 +0700

    Add: screen settings

--- Project-MusicStreamApp-Frogs/app/Navigation/ProfileStack.js
+++ Project-MusicStreamApp-Frogs/app/Navigation/ProfileStack.js
@@ -4,6 +4,8 @@ import Profile from '../screens/profile/Profile';
 import { Colors } from '../../constants/Colors';
 import ProfileHeaderLeft from '../screens/profile/ProfileHeaderLeft';
 import ProfileHeaderRight from '../screens/profile/ProfileHeaderRight';
+import Settings from '../screens/settings/Settings';
+import SettingRight from '../screens/settings/SettingRight';
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +27,20 @@ const ProfileStack = () => {
                     ),
                     headerRight: () => (
                         <ProfileHeaderRight />
+                    ),
+                 }}
+            />
+            <Stack.Screen
+                name="Settings"
+                component={Settings}
+                options={{ 
+                    headerStyle: {
+                        backgroundColor: Colors.neutral.rgba1,
+                    },
+                    headerTintColor: Colors.neutral.white,
+                    headerShadowVisible: false,
+                    headerRight: () => (
+                        <SettingRight/>
                     ),
                  }}
             />
