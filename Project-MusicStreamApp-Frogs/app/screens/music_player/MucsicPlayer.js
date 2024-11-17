f6b07c6bdf076157738c96c0714b6ed842e77a54
phan dinh thai <phandinhthai012@gmail.com>
2024-11-17 17:47:09 +0700

    Update: screen MusicPlayer

--- Project-MusicStreamApp-Frogs/app/screens/music_player/MucsicPlayer.js
+++ Project-MusicStreamApp-Frogs/app/screens/music_player/MucsicPlayer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground, ScrollView } from 'react-native'
 import React from 'react'
 import { Popins } from '@/constants/Popins';
 import { Colors } from '@/constants/Colors';
@@ -8,16 +8,18 @@ import { Ionicons } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
+import {styles} from './style/MucsicPlayer'
 
 
 const MucsicPlayer = () => {
     return (
+        
         <View style={styles.container}>
             <ImageBackground
                 source={require('../../../assets/images/Imgtheme2.png')}
                 style={{ width: '100%', height: '100%' }}
             >
-                <View style={styles.content}>
+                <ScrollView style={styles.content}>
                     <View style={styles.header}>
                         <TouchableOpacity style={styles.btndown}>
                             <AntDesign name="down" size={24} color="white" style={styles.icon} />
@@ -84,104 +86,10 @@ const MucsicPlayer = () => {
                             </View>
                         </View>
                     </View>
-
-                </View>
+                </ScrollView>
             </ImageBackground>
-            <Text>Music Player</Text>
         </View>
     )
 }
 
 export default MucsicPlayer
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: Colors.neutral.gray,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    content: {
-        flex: 1,
-        width: '100%',
-        height: '100%',
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginHorizontal: 20,
-        marginTop: 40,
-    },
-    btndown: {
-        backgroundColor: 'rgba(255, 255, 256, 0.3)',
-        padding: 10,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    bodycontent: {
-        flex: 1,
-        top: 300,
-    },
-    component: {
-        marginHorizontal: 20,
-        marginTop: 28,
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginTop: 5
-    },
-    colorwhite: {
-        color: Colors.neutral.white,
-    },
-    color2: {
-        color: 'rgba(255, 255, 255, 0.5)',
-    },
-    rowitem: {
-        marginVertical: 10,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: Colors.neutral.white,
-        marginVertical: 5,
-    },
-    subTitle: {
-        fontSize: 14,
-    },
-    btnPlay: {
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        // padding: 10,
-        paddingHorizontal: 15,
-        paddingVertical: 8,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    viewicon: {
-        flexDirection: 'row',
-        // justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    btnicon: {
-        backgroundColor: 'rgba(255, 255, 255, 0.2)',
-        padding: 14,
-        borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    marginright:{
-        marginRight: 10,
-    },
-    controllplayer: {
-        marginHorizontal: 36,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-
-})
--- Project-MusicStreamApp-Frogs/app/screens/music_player/style/MucsicPlayer.js
+++ Project-MusicStreamApp-Frogs/app/screens/music_player/style/MucsicPlayer.js
@@ -1,3 +1,97 @@
 import { StyleSheet } from "react-native";
 import { Popins } from '@/constants/Popins';
 import { Colors } from '@/constants/Colors';
+
+
+export const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: Colors.neutral.gray,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        marginTop: 40,
+    },
+    btndown: {
+        backgroundColor: 'rgba(255, 255, 256, 0.3)',
+        padding: 10,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bodycontent: {
+        flex: 1,
+        top: 300,
+    },
+    component: {
+        marginHorizontal: 20,
+        marginBottom: 20,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 5
+    },
+    colorwhite: {
+        color: Colors.neutral.white,
+    },
+    color2: {
+        color: 'rgba(255, 255, 255, 0.5)',
+    },
+    rowitem: {
+        marginVertical: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: Colors.neutral.white,
+        marginVertical: 5,
+    },
+    subTitle: {
+        fontSize: 14,
+    },
+    btnPlay: {
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        // padding: 10,
+        paddingHorizontal: 15,
+        paddingVertical: 8,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    viewicon: {
+        flexDirection: 'row',
+        // justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    btnicon: {
+        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+        padding: 14,
+        borderRadius: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    marginright:{
+        marginRight: 10,
+    },
+    controllplayer: {
+        height: 180,
+        marginBottom: 20,
+        marginHorizontal: 36,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'flex-start',
+    },
+})
