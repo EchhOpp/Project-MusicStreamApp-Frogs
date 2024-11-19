806595693a8c7a2526080e21c946db1f9de65bd8
phan dinh thai <phandinhthai012@gmail.com>
2024-11-20 01:43:20 +0700

    Update: screen related

--- Project-MusicStreamApp-Frogs/app/screens/related/Related.js
+++ Project-MusicStreamApp-Frogs/app/screens/related/Related.js
@@ -5,7 +5,7 @@ import Feather from 'react-native-vector-icons/Feather';
 import ListListenMusic from '@/components/ListListenMusic';
 import PlayList from '@/components/PlayList';
 
-// import styles from './style/Related'
+import styles from './style/Related'
 
 const Related = () => {
     const data = [
@@ -174,80 +174,80 @@ const Related = () => {
 
 export default Related
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        marginBottom: 120
-    },
-    gradientOverlay: {
-        flex: 1,
-    },
-    Container: {
-        marginHorizontal: 20,
-        marginTop: 40
-    },
-    header: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20
-    },
-    btndown: {
-        padding: 10,
-        backgroundColor: 'rgba(255,255,255,0.2)',
-        borderRadius: 50
-    },
-    list: {
-    },
-    ItemsHeader: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 20
-    },
-    titles: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white'
-    },
-    btnmore: {
-        borderColor: 'rgba(255, 255, 255, 0.2)',
-        borderWidth: 1,
-        borderRadius: 50,
-        paddingHorizontal: 16,
-        paddingVertical: 6,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    more: {
-        color: 'white',
-        fontSize: 16,
-    },
-    RelatedSongs: {
-        marginTop: 10
-    },
-    albums: {
-        marginTop: 20
-    },
-    listenagain: {
-        marginTop: 10
-    },
-    SimillarArtists: {
-        marginTop: 10
-    },
-    Artist: {
-        alignItems: 'center',
-        marginRight: 10
-    },
-    image: {
-        width: 80,
-        height: 80,
-        borderRadius: 50
-    },
-    title: {
-        fontSize: 16,
-        color: 'rgba(255, 255, 255, 0.8)',
-        width: 90,
-        textAlign: 'center'
-    }
-})
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         marginBottom: 120
+//     },
+//     gradientOverlay: {
+//         flex: 1,
+//     },
+//     Container: {
+//         marginHorizontal: 20,
+//         marginTop: 40
+//     },
+//     header: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         marginBottom: 20
+//     },
+//     btndown: {
+//         padding: 10,
+//         backgroundColor: 'rgba(255,255,255,0.2)',
+//         borderRadius: 50
+//     },
+//     list: {
+//     },
+//     ItemsHeader: {
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//         alignItems: 'center',
+//         marginBottom: 20
+//     },
+//     titles: {
+//         fontSize: 20,
+//         fontWeight: 'bold',
+//         color: 'white'
+//     },
+//     btnmore: {
+//         borderColor: 'rgba(255, 255, 255, 0.2)',
+//         borderWidth: 1,
+//         borderRadius: 50,
+//         paddingHorizontal: 16,
+//         paddingVertical: 6,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+//     more: {
+//         color: 'white',
+//         fontSize: 16,
+//     },
+//     RelatedSongs: {
+//         marginTop: 10
+//     },
+//     albums: {
+//         marginTop: 20
+//     },
+//     listenagain: {
+//         marginTop: 10
+//     },
+//     SimillarArtists: {
+//         marginTop: 10
+//     },
+//     Artist: {
+//         alignItems: 'center',
+//         marginRight: 10
+//     },
+//     image: {
+//         width: 80,
+//         height: 80,
+//         borderRadius: 50
+//     },
+//     title: {
+//         fontSize: 16,
+//         color: 'rgba(255, 255, 255, 0.8)',
+//         width: 90,
+//         textAlign: 'center'
+//     }
+// })
